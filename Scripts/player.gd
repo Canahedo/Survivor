5ec4dab7e7fa4232418800361a363a4e8f52f400
Canahedo <Canahedo@gmail.com>
2025-01-10 22:16:45 -0500

    Player animations with physics movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,61 +1,23 @@
 extends CharacterBody2D
 
-# Animation Player child node
-@onready var animation_player = $AnimatedSprite2D
+@onready var animation = $AnimatedSprite2D
 
 # Properties
-@export var speed = 150.0
-var face_direction: String
-var animation_to_play: String
-
-# Start front idle animation on load
-func _ready():
-	animation_player.stop()
-	animation_player.play("walk_down")
-
-func _physics_process(_delta):
-	# Reset velocity
-	velocity = Vector2.ZERO
-	
-	# Add appropriate velocities depending on button press
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1.0 * speed
-		# Only face left/right if not diagonal movement
-		if velocity.y == 0.0:
-			face_direction = "left"
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1.0 * speed
-		# Only face left/right if not diagonal movement
-		if velocity.y == 0.0:
-			face_direction = "right"
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1.0 * speed
-		face_direction = "up"
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1.0 * speed
-		face_direction = "down"
-		
-	animation_to_play =  "walk_" + face_direction
-	animation_player.play(animation_to_play)
-	
-	# Move character, slide at collision
-	move_and_slide()
-
-
-
-"""
-# Acceleration and friction movement
-# No clue how to get it to work with the animations
-
-@export var max_speed = 100
-@export var accel = 50
-@export var friction = 50
-
+@export var max_speed: int = 125
+@export var accel: int = 1000
+@export var friction: int = 1000
+@export var player_is_carrying: bool = false
 var input = Vector2.ZERO
-var player_is_carrying = false
+
+# Start down animation on load
+func _ready():
+	animation.stop()
+	animation.play("walk_down")
 
 func _physics_process(delta):
 	player_movement(delta)
+	move_and_slide()
+	update_animation()
 	
 func get_input():
 	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
@@ -73,6 +35,21 @@ func player_movement(delta):
 	else:
 		velocity += (input * accel * delta)
 		velocity = velocity.limit_length(max_speed)
+		
+func update_animation():
 	
-	move_and_slide()
-"""
+	var direction: String
+	if abs(velocity.x) > abs(velocity.y):
+		if velocity.x < 0: direction = "left"
+		elif velocity.x > 0: direction = "right"
+	elif abs(velocity.y) > abs(velocity.x):
+		if velocity.y < 0: direction = "up"
+		if velocity.y > 0: direction = "down" 
+	
+	if velocity.x != 0 or velocity.y != 0:
+		if player_is_carrying:
+			animation.play("carry_" + direction)
+		else:
+			animation.play("walk_" + direction)
+	else:
+		animation.stop()
