8628710750c1c2387d82b0154dcc825007db5a03
Canahedo <Canahedo@gmail.com>
2025-01-10 21:40:39 -0500

    Player movement animations
    Replaced friction and acceleration based movement with animations. Will try to re-implement friction/acceleration eventually.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,58 @@
 extends CharacterBody2D
 
+# Animation Player child node
+@onready var animation_player = $AnimatedSprite2D
+
+# Properties
+@export var speed = 150.0
+var face_direction: String
+var animation_to_play: String
+
+# Start front idle animation on load
+func _ready():
+	animation_player.stop()
+	animation_player.play("walk_down")
+
+func _physics_process(_delta):
+	# Reset velocity
+	velocity = Vector2.ZERO
+	
+	# Add appropriate velocities depending on button press
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1.0 * speed
+		# Only face left/right if not diagonal movement
+		if velocity.y == 0.0:
+			face_direction = "left"
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1.0 * speed
+		# Only face left/right if not diagonal movement
+		if velocity.y == 0.0:
+			face_direction = "right"
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1.0 * speed
+		face_direction = "up"
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1.0 * speed
+		face_direction = "down"
+		
+	animation_to_play =  "walk_" + face_direction
+	animation_player.play(animation_to_play)
+	
+	# Move character, slide at collision
+	move_and_slide()
+
+
+
+"""
+# Acceleration and friction movement
+# No clue how to get it to work with the animations
+
 @export var max_speed = 100
 @export var accel = 50
 @export var friction = 50
 
 var input = Vector2.ZERO
+var player_is_carrying = false
 
 func _physics_process(delta):
 	player_movement(delta)
@@ -27,4 +75,4 @@ func player_movement(delta):
 		velocity = velocity.limit_length(max_speed)
 	
 	move_and_slide()
-	
+"""
