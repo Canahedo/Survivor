301c854d9eae08bcc1def200247e0e67999d6ebb
Canahedo <Canahedo@gmail.com>
2025-01-10 20:28:28 -0500

    Catching branch up to changes on main

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,30 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+@export var max_speed = 100
+@export var accel = 50
+@export var friction = 50
 
-func _process(delta):
+var input = Vector2.ZERO
+
+func _physics_process(delta):
+	player_movement(delta)
 	
-	# input
-	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * SPEED * delta
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
+	
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta) 
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(max_speed)
+	
 	move_and_slide()
+	
