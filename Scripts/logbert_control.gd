18972a95f31de7e502f378b6dfcd9da2926f1432
Joe <wingusinbound@gmail.com>
2025-01-11 23:48:49 -0500

    updated logbert spawning
    logbert now spawns outside camera view and on platform.

--- Scripts/logbert_control.gd
+++ Scripts/logbert_control.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 # Onready
 @onready var logbert_scene: PackedScene = preload("res://Scenes/logbert.tscn")
+@onready var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+@onready var path: PathFollow2D = get_node("/root/Main/Player/Path2D/PathFollow2D")
 
 
 # Variables
@@ -11,6 +13,8 @@ const logbert_speed_modifier: int = 10 # Controls how quickly the speed increase
 const logbert_cooldown_modifier: float = 1.0 # How much the cooldown timer is reduced each respawn, in seconds
 const logberts_per_cycle: int = 5 # How many Logberts spawn before the cooldown is reduced
 
+func _ready() -> void:
+	rng.randomize()
 
 # Spawns Logberts on a timer, increases their speed and reduces the cooldown each time
 func _on_logbert_spawn_timer_timeout():
@@ -21,9 +25,13 @@ func _on_logbert_spawn_timer_timeout():
 
 
 func spawn_logbert():
-	var logbert_instance = logbert_scene.instantiate()
-	logbert_instance.position = Vector2(randi_range(0, 640), randi_range(0, 360))
+	var logbert_instance: CharacterBody2D = logbert_scene.instantiate()
+	path.progress_ratio = 1
+	var path_progress_max = path.progress
+	path.progress = rng.randi_range(0,path_progress_max)
+	while((path.global_position.x < 0) or (path.global_position.x > 640) or (path.global_position.y < 0) or (path.global_position.y > 360)):
+		path.progress = rng.randi_range(0,path_progress_max)
+	logbert_instance.position = path.global_position
+	#logbert_instance.position = Vector2(randi_range(0, 640), randi_range(0, 360))
 	logbert_instance.max_speed += logbert_speed_modifier
 	add_child(logbert_instance)
-	
-	
