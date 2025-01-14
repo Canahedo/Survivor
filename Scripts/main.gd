ca12b10947a4c1e0af82732aad65baa218eea3da
Joe <wingusinbound@gmail.com>
2025-01-13 23:12:08 -0500

    added method for updateing navigation polygon and baking

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -8,15 +8,8 @@ var paused: bool = false
 
 
 func _ready():
-	print(nav_region.navigation_polygon.get_outline(0))
 	pause_menu.hide()
-	var play_area: PackedVector2Array = get_game_area()
-	var nav_polygon: NavigationPolygon = NavigationPolygon.new()
-	nav_polygon.set_vertices(play_area)
-	var new_polygon_indicies = PackedInt32Array([0, 1, 2, 3])
-	nav_polygon.get_navigation_mesh().add_polygon(new_polygon_indicies)
-	nav_region.navigation_polygon = nav_polygon
-	NavigationServer2D.region_set_navigation_polygon(nav_region.get_rid(), nav_region.navigation_polygon)
+	update_nav_polygon()
 
 
 func _process(_delta) -> void:
@@ -33,7 +26,7 @@ func pauseMenu():
 		Engine.time_scale = 0
 	
 	paused = !paused
-	
+
 
 func get_game_area() -> PackedVector2Array:
 	var game_area = []
@@ -51,3 +44,13 @@ func get_game_area() -> PackedVector2Array:
 	var game_area_packed_vector = PackedVector2Array([top_right, bottom_left, bottom_right, top_left])
 	
 	return game_area_packed_vector
+
+
+func update_nav_polygon() -> void:
+	var play_area: PackedVector2Array = get_game_area()
+	var nav_polygon: NavigationPolygon = NavigationPolygon.new()
+	nav_polygon.set_vertices(play_area)
+	var new_polygon_indicies = PackedInt32Array([0, 1, 2, 3])
+	nav_polygon.get_navigation_mesh().add_polygon(new_polygon_indicies)
+	nav_region.navigation_polygon = nav_polygon
+	NavigationServer2D.region_set_navigation_polygon(nav_region.get_rid(), nav_region.navigation_polygon)
