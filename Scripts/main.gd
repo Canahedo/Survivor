c60954b9e6a6980d17695fb0519909621165d29e
Joe <wingusinbound@gmail.com>
2025-01-13 23:04:09 -0500

    update map navigation region at runtime

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 @onready var tile_map: TileMapLayer = $EnemyNavRegion/TileMapLayer
 @onready var pause_menu: Control = $CanvasLayer/PauseMenu
 @onready var nav_region: NavigationRegion2D = $EnemyNavRegion
@@ -7,16 +8,16 @@ var paused: bool = false
 
 
 func _ready():
+	print(nav_region.navigation_polygon.get_outline(0))
 	pause_menu.hide()
 	var play_area: PackedVector2Array = get_game_area()
-	var new_navigation_mesh = NavigationPolygon.new()
-	var new_vertices = play_area
-	new_navigation_mesh.vertices = new_vertices
-	var new_polygon_indices = PackedInt32Array([0, 1, 2, 3])
-	new_navigation_mesh.add_polygon(new_polygon_indices)
-	nav_region.navigation_polygon = new_navigation_mesh
-	NavigationServer2D.bake_from_source_geometry_data(new_navigation_mesh, NavigationMeshSourceGeometryData2D.new());
-	nav_region.bake_navigation_polygon()
+	var nav_polygon: NavigationPolygon = NavigationPolygon.new()
+	nav_polygon.set_vertices(play_area)
+	var new_polygon_indicies = PackedInt32Array([0, 1, 2, 3])
+	nav_polygon.get_navigation_mesh().add_polygon(new_polygon_indicies)
+	nav_region.navigation_polygon = nav_polygon
+	NavigationServer2D.region_set_navigation_polygon(nav_region.get_rid(), nav_region.navigation_polygon)
+
 
 func _process(_delta) -> void:
 	if Input.is_action_just_pressed("pause"):
@@ -36,16 +37,17 @@ func pauseMenu():
 
 func get_game_area() -> PackedVector2Array:
 	var game_area = []
+	var pixels = tile_map.rendering_quadrant_size
 	var game_rect = tile_map.get_used_rect()
 	for y in range(game_rect.position.y, game_rect.end.y):
 		for x in range(game_rect.position.x, game_rect.end.x):
 			var cell_terrain = tile_map.get_cell_tile_data(Vector2i(x,y)).get_custom_data("Terrain")
 			if cell_terrain == 1:
 				game_area.append(Vector2i(x,y))
-	var top_right = Vector2i(game_area[0])
-	var top_left = Vector2i(game_area[game_area.size() - 1].x, game_area[0].y)
-	var bottom_left = Vector2i(game_area[0].x, game_area[game_area.size() - 1].y)
-	var bottom_right = Vector2i(game_area[game_area.size() - 1])
+	var top_right = Vector2(game_area[0].x * pixels, game_area[0].y * pixels)
+	var top_left = Vector2(game_area[game_area.size() - 1].x * pixels, game_area[0].y * pixels)
+	var bottom_left = Vector2(game_area[0].x * pixels, game_area[game_area.size() - 1].y * pixels)
+	var bottom_right = Vector2(game_area[game_area.size() - 1].x * pixels, game_area[game_area.size() - 1].y * pixels)
 	var game_area_packed_vector = PackedVector2Array([top_right, bottom_left, bottom_right, top_left])
 	
 	return game_area_packed_vector
