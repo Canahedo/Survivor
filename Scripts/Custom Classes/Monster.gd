80de24d41095adf772b8223158ba8064fa76a0e6
Canahedo <Canahedo@gmail.com>
2025-01-20 16:20:19 -0500

    Enemy movements

--- Scripts/Custom Classes/Monster.gd
+++ Scripts/Custom Classes/Monster.gd
@@ -7,14 +7,18 @@ class_name Monster
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var nav_refresh: Timer = $NavRefresh
 
+var is_alert: bool = false
+var alert_range: int # Set per monster
+
 
 # If _ready() function is overwritten on inherited script, ensure to keep delay_physics()
 func _ready() -> void:
-	self.max_speed = 50
-	delay_physics()
+	pass
+	#delay_physics()
 
 
 # Pauses physics processing until fully set up
+# Not currently being used, may be unnecessary
 func delay_physics() -> void:
 	set_physics_process(false)
 	await get_tree().process_frame
@@ -22,8 +26,9 @@ func delay_physics() -> void:
 	set_physics_process(true)
 
 
-func _physics_process(delta: float) -> void:
+func move_toward_player(delta: float) -> void:
 	var path_direction: Vector2 = to_local(nav_agent.get_next_path_position()).normalized()
+	self.dir_facing = path_direction
 	self.velocity += (path_direction * accel * delta)
 	self.velocity = velocity.limit_length(max_speed)
 	var _collided: bool = move_and_slide()
@@ -32,6 +37,8 @@ func _physics_process(delta: float) -> void:
 # Gets player global position and updates target position
 func get_path_to_player() -> void:
 	nav_agent.target_position = player.global_position
+	if nav_agent.distance_to_target() <= alert_range:
+		is_alert = true
 	if nav_agent.distance_to_target() <= nav_agent.target_desired_distance:
 		nav_agent.target_position = self.global_position
 
--- Scripts/logbert.gd
+++ Scripts/logbert.gd
@@ -1,2 +1,15 @@
 extends Monster
 class_name Logbert
+
+
+func _ready() -> void:
+	alert_range = 50
+	animation_tree.active = true
+	animation_tree.set("parameters/TimeScale/scale", 1.0)
+	self.max_speed = 50
+	#delay_physics()
+
+func _physics_process(delta: float) -> void:
+	if is_alert:
+		move_toward_player(delta)
+	animation_tree.set("parameters/StateMachine/Walk/blend_position", dir_facing)
