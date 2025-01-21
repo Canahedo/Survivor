c41cddd3827729bafbd5e0897170b69c68e9f1ce
Joe <wingusinbound@gmail.com>
2025-01-20 20:47:11 -0500

    added restart option to pause menu

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -18,3 +18,7 @@ func _on_resume_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
+	toggle_pause()
